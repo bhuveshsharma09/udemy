53c9541440f1f9b9d88c45a99cb22ddbf9b9e193
bhuvesh kumar <bhuveshsharma09@gmail.com>
2020-07-02 19:40:38 +0800

    new

---
+++ app.js
@@ -0,0 +1,31 @@
+var height;
+var mass;
+
+var markBMI, johnBMI;
+
+height = 1.73;
+mass = 68;
+
+markBMI = mass / (height * height);
+
+height = 1.67;
+mass = 77;
+
+johnBMI = mass / (height * height)
+
+console.log(markBMI,johnBMI)
+
+var compare;
+compare = markBMI > johnBMI
+
+console.log("comparison result ",compare)
+
+if (height === 1.77)
+{
+  console.log('rolla pe gaya')
+}
+else
+{
+  console.log('nahi pada')
+}
+
